3ef63db2438d5c90ea885be5588b8029dfd38dae
Rkrohit371 <rohitkg1998@gmail.com>
2020-08-13 22:47:45 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.indigo[700],
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(child: Text('Tap on Dice')),
+          backgroundColor: Colors.indigo[900],
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNum = 1;
+  int rightNum = 1;
+
+  void tapDice() {
+    setState(() {
+      leftNum = Random().nextInt(6) + 1;
+      rightNum = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: tapDice,
+              child: Image.asset('images/dice$leftNum.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: tapDice,
+              child: Image.asset('images/dice$rightNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
